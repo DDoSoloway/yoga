0df318c3b9d1fbacba4e5b8f73aeadbe8d9072ef
Вячеслав Агеев <netenitikolokola@gmail.com>
2019-12-11 06:46:37 +0300

    add function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-window.addEventListener('load');
+window.addEventListener('DOMContentLoaded', function() {
+
+        'use strict';
+        let tab = document.querySelectorAll('.info-header-tab'),
+            info = document.querySelector('.info-header'),
+            tabContent = document.querySelectorAll('.info-tabcontent');
+});
